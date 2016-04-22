bdeff07485c2568ac131bb0afea97cab9083532a
pyavchik <pyavchik@gmail.com>
2016-04-22 23:18:48 +0300

    changes for https://www.refheap.com/118008

--- src/test/java/work2/TodoMVCTest.java
+++ src/test/java/work2/TodoMVCTest.java
@@ -1,8 +1,13 @@
 package work2;
 
+import com.codeborne.selenide.ElementsCollection;
 import org.junit.Test;
 
 import static com.codeborne.selenide.CollectionCondition.empty;
+import static com.codeborne.selenide.CollectionCondition.exactTexts;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TodoMVCTest extends BaseTest {
@@ -12,17 +17,51 @@ public class TodoMVCTest extends BaseTest {
         open("https://todomvc4tasj.herokuapp.com/");
 
         add("1", "2", "3", "4");
-        assertTasksAre("1", "2", "3", "4");
+        assertTasks("1", "2", "3", "4");
 
         delete("2");
-        assertTasksAre("1", "3", "4");
+        assertTasks("1", "3", "4");
 
         toggle("4");
         clearCompleted();
-        assertTasksAre("1", "3");
+        assertTasks("1", "3");
 
         toggleAll();
         clearCompleted();
+        assertTasks();
+    }
+
+    private void assertTasks() {
         tasks.shouldBe(empty);
+    }
+
+    private void assertTasks(String... taskTexts) {
+        tasks.shouldHave(exactTexts(taskTexts));
+    }
+
+    private void add(String... taskTexts) {
+        for (String text : taskTexts) {
+            $("#new-todo").setValue(text).pressEnter();
+        }
+    }
+
+    private ElementsCollection tasks = $$("#todo-list li");
+
+    private void clearCompleted() {
+        $("#clear-completed").click();
+    }
+
+    private void delete(String taskText) {
+        tasks.find(exactText(taskText)).hover().find(".destroy").click();
+    }
+
+    //complete
+    private void toggle(String taskText) {
+        tasks.find(exactText(taskText)).find(".toggle").click();
+    }
+
+    //complete all
+    private void toggleAll() {
+        $("#toggle-all").click();
     }
 }
